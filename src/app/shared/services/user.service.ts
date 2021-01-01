7a7f13503c626f706ee3ccb87369a1f436862e10
astreagoh <76113005+astreagoh@users.noreply.github.com>
2021-01-01 12:51:49 +0800

    Changes
    Made changes by adding a optional image field and when registering the field is empty. Additionally, added 2 methods, one to read the data in firebase and one to update fields for user

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -5,6 +5,7 @@ import	'firebase/analytics';
 import  'firebase/auth'
 import  'firebase/firestore';
 import  'firebase/storage';
+import { Observable } from 'rxjs';
 
 
 
@@ -32,12 +33,34 @@ export class UserService {
       gender: p.gender,
       email: p.email,
       password: p.password,
-      phoneno: p.phoneno
+      phoneno: p.phoneno,
+      image: ""
     });
   }
 
   logout()	{	
     return	firebase.auth().signOut();	
+  }
+
+  updateProfile(p:User)	{	
+    const ref = firebase.firestore().collection('users').doc(p.email);
+    ref.update({
+      name: p.name,
+      address: p.address,
+      birthday: new Date(p.birthday),
+      gender: p.gender,
+      phoneno: p.phoneno
+    });
+  }
+
+  getUserInfoNoImage(id:string): Observable<any> {
+    return new Observable(observer => {
+      // Read collection '/users'
+      firebase.firestore().collection('users').doc(id).get().then((doc) => {
+        let loan = new User(doc.data().name, doc.data().gender, doc.data().birthday, doc.data().email, doc.data().password, doc.data().phoneno, doc.data().address);
+        observer.next(loan);
+      });
+    });
   }
   
 }
