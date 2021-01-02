264101a6d6c0f218d50e50e3fe43a39490a2ec69
benedict tay <tayhaozeben@gmail.com>
2021-01-02 17:43:37 +0800

    Added upload photo feature to signup

---
+++ src/app/upload-photo/upload-photo.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { UploadPhotoPage } from './upload-photo.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UploadPhotoPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [UploadPhotoPage]
+})
+export class UploadPhotoPageModule {}
