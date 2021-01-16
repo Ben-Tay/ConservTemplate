ff47ea937b01c7c172d62a6b54737b21bda9ee3a
benedict tay <tayhaozeben@gmail.com>
2021-01-16 11:34:31 +0800

    Added Accept Applicants Feature

--- src/app/clientjobsnotification/clientjobsnotification.page.ts
+++ src/app/clientjobsnotification/clientjobsnotification.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { ErrandRunner } from '../shared/models/ErrandRunner';
 import { Job } from '../shared/models/Job';
 import { JobService } from '../shared/services/job.service';
@@ -14,8 +15,9 @@ export class ClientjobsnotificationPage implements OnInit {
   jobid: string;
   job: Job;
   jobapplicants: ErrandRunner[]
+  
 
-  constructor(private route: ActivatedRoute, private jobservice: JobService, private userservice: UserService) { 
+  constructor(private route: ActivatedRoute, private jobservice: JobService, private userservice: UserService, private router: Router, private toastCtrl: ToastController) { 
     this.jobid = this.route.snapshot.params.id;
 
     this.jobservice.getSpecificJobsById(this.jobid)
@@ -29,6 +31,34 @@ export class ClientjobsnotificationPage implements OnInit {
   ngOnInit() {
     this.userservice.showLoading();
   }
+
+  toERProfile(id: string){
+      this.router.navigate(['/userprofile', id])
+  }
+
+  async AcceptApplicant(applicant: ErrandRunner){
+    this.jobservice.getSpecificJobsById(this.jobid)
+    .then(data => {
+      this.job = data;
+    })
+
+    //Move document from JobsAvailable Collection to JobsAccepted Collection
+    this.jobservice.acceptapplicantrequest(this.job, applicant)
+    .then(() => {
+      this.jobservice.deletefromJobsAvailable(this.job)
+    })
+
+    let toast = await this.toastCtrl.create({
+      message: "You have accepted this errand request",
+      position: 'top',
+      duration: 2000
+    })
+    toast.present()
+
+    this.router.navigate(['jobs-accepted'])
+    
+  }
+ 
   
 
 }
---
+++ src/app/jobs-accepted/jobs-accepted.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs-accepted',
+  templateUrl: './jobs-accepted.page.html',
+  styleUrls: ['./jobs-accepted.page.scss'],
+})
+export class JobsAcceptedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
