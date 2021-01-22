57738752d537718e5953ec3a0bcd1045422c9659
astreagoh <76113005+astreagoh@users.noreply.github.com>
2021-01-22 18:49:18 +0800

    Rejected Section

--- src/app/shared/services/job-er.service.ts
+++ src/app/shared/services/job-er.service.ts
@@ -96,6 +96,41 @@ export class JobERService {
     });
   }
 
+  getAllErrandsRejected(id: string): Observable<any> {
+    return new Observable(observer => {
+      // Read collection '/JobsAvailable'
+      const ref = firebase.firestore().collection('JobsAvailable')
+      ref.onSnapshot(collection => {
+        let array = [];
+        
+        collection.forEach(doc => {
+          // Add jobs into array if there's no error
+          try {
+            const docRef = ref.doc(doc.id)
+            docRef.collection('Applicants').where('applicationstatus', '==', 'Rejected').get().then(sdoc => {
+              let applied = null
+              sdoc.forEach(ssdoc => {
+                if (ssdoc.id === id) {
+                  applied = true
+                }
+                else{
+                  applied = false
+                }
+
+                if (applied === true) {
+                  let loan = new Job(doc.data().errandname, doc.data().category, doc.data().status, doc.data().client, doc.data().date.toDate(), doc.data().description, doc.data().time, doc.id, doc.data().price);
+                  array.push(loan);
+                }
+              })
+            })
+          } catch (error) { }
+
+        });
+        observer.next(array);
+      });
+    });
+  }
+
   getAllErrandsAccepted(id: string): Observable<any> {
     return new Observable(observer => {
       // Read collection '/JobsAccepted'
