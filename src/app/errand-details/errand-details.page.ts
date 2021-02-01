14a5ed25c131244492d9e4a8865e0f6a0345ccc5
astreagoh <76113005+astreagoh@users.noreply.github.com>
2021-02-01 08:12:36 +0800

    Applied Clashing Check

--- src/app/errand-details/errand-details.page.ts
+++ src/app/errand-details/errand-details.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { async } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { isThisISOWeek } from 'date-fns';
 import { ErrandRunner } from '../shared/models/ErrandRunner';
 import { Job } from '../shared/models/Job';
 import { JobERService } from '../shared/services/job-er.service';
@@ -17,15 +19,28 @@ export class ErrandDetailsPage implements OnInit {
   job: Job;
   user: string;
 
-  constructor(private jobService: JobService, private jobERService: JobERService, private route: ActivatedRoute, 
+  array: Job[] = [];
+  acceptedarray: Job[] = [];
+
+  constructor(private jobService: JobService, private jobERService: JobERService, private route: ActivatedRoute,
     private userService: UserService, private router: Router, private toastController: ToastController) {
     this.errandId = this.route.snapshot.params.id;
-    
+
     this.jobService.getSpecificJobsById(this.errandId)
-    .then(data =>{
-      this.job = data
-      this.userService.getUserInfoNoImage(this.job.client).subscribe(data=>{
-        this.user = data.name
+      .then(data => {
+        this.job = data
+        this.userService.getUserInfoNoImage(this.job.client).subscribe(data => {
+          this.user = data.name
+        })
+      })
+
+    this.userService.observeAuthState(user => {
+      this.jobERService.getAllErrandsApplied(user.email).subscribe(data => {
+        this.array = data
+      })
+
+      this.jobERService.getAllErrandsAccepted(user.email).subscribe(data=>{
+        this.acceptedarray = data
       })
     })
   }
@@ -34,29 +49,97 @@ export class ErrandDetailsPage implements OnInit {
     this.userService.showLoading();
   }
 
-  applyErrand(){
-    this.userService.observeAuthState(user=>{
-      if(user){
-        if (!(user.email === this.job.client)){
-          let ERdetails = new ErrandRunner(new Date(), user.email, 'Pending')
-          this.jobERService.applyjobs(this.errandId, ERdetails).then(async retrieve=>{
-            if(retrieve == true){
-              const toast = await this.toastController.create({
-                message: 'You have already applied for this errand',
-                duration: 2000,
-                position: 'top',
-                color: 'secondary'
-              });
-              toast.present();
+  applyErrand() {
+    //Check user is logged in
+    this.userService.observeAuthState(user => {
+      if (user) {
+        let ERdetails = new ErrandRunner(new Date(), user.email, 'Pending')
+
+        //If array is not 0 then check
+        if(this.acceptedarray.length != 0){
+          this.acceptedarray.forEach(async mdoc=>{
+            if (mdoc.date.getDate() != this.job.date.getDate()) {
+              if (this.array.length != 0) {
+                this.array.forEach(async doc => {
+                  if (doc.date.getDate() != this.job.date.getDate()) {
+                    this.successfullyapplied(ERdetails)
+                  }
+                  else {
+                    if(doc.time.getTime()>=this.job.endtime.getTime()||doc.endtime.getTime()<=this.job.time.getTime()){
+                      this.successfullyapplied(ERdetails)
+                    }
+                    else{
+                      const toast = await this.toastController.create({
+                        message: 'This job is clashing with a job you have applied',
+                        duration: 2000,
+                        position: 'top',
+                        color: 'secondary'
+                      });
+                      toast.present();
+                      this.router.navigate(['/all-errand-requests'])
+                    }
+                  }
+                })
+              }
+              else {
+                this.successfullyapplied(ERdetails)
+              }
+            }
+            else {
+              if(mdoc.time.getTime()>=this.job.endtime.getTime() || mdoc.endtime.getTime()<=this.job.time.getTime()){
+                if (this.array.length != 0) {
+                  this.array.forEach(async doc => {
+                    if (doc.date.getDate() != this.job.date.getDate()) {
+                      this.successfullyapplied(ERdetails)
+                    }
+                    else {
+                      if(doc.time.getTime()>=this.job.endtime.getTime() || doc.endtime.getTime()<=this.job.time.getTime()){
+                        this.successfullyapplied(ERdetails)
+                      }
+                      else{
+                        const toast = await this.toastController.create({
+                          message: 'This job is clashing with a job you have applied',
+                          duration: 2000,
+                          position: 'top',
+                          color: 'secondary'
+                        });
+                        toast.present();
+                        this.router.navigate(['/all-errand-requests'])
+                      }
+                    }
+                  })
+                }
+                else {
+                  this.successfullyapplied(ERdetails)
+                }
+              }
+              else{
+                const toast = await this.toastController.create({
+                  message: 'This job is clashing with a job you have',
+                  duration: 2000,
+                  position: 'top',
+                  color: 'secondary'
+                });
+                toast.present();
+                this.router.navigate(['/all-errand-requests'])
+              }
             }
           })
-          this.router.navigate(['/all-errand-requests'])
         }
       }
     })
   }
 
-  redirectprofile(){
-    this.router.navigate(['/userprofile', this.job.client])
+  successfullyapplied(ER: ErrandRunner) {
+    return this.jobERService.applyjobs(this.errandId, ER).then(async retrieve => {
+      const toast = await this.toastController.create({
+        message: 'You have applied for this job',
+        duration: 2000,
+        position: 'top',
+        color: 'secondary'
+      });
+      toast.present();
+      this.router.navigate(['/all-errand-requests'])
+    })
   }
 }
