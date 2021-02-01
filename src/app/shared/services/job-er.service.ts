a5b3954d1ad2f691ef9ba91d83e314e0f92bd1c5
astreagoh <76113005+astreagoh@users.noreply.github.com>
2021-02-01 23:39:55 +0800

    Update job-er.service.ts

--- src/app/shared/services/job-er.service.ts
+++ src/app/shared/services/job-er.service.ts
@@ -22,16 +22,18 @@ export class JobERService {
 
           // Add jobs into array if there's no error
           try {
-            const docRef = ref.doc(doc.id)
-            docRef.collection('Applicants').doc(id).get().then(sdoc => {
-              if (sdoc.exists) {
-                return;
-              }
-              else {
-                let loan = new Job(doc.data().errandname, doc.data().category, doc.data().status, doc.data().client, doc.data().date.toDate(), doc.data().description, doc.data().time.toDate(), doc.data().endtime.toDate(), doc.id, doc.data().price);
-                array.push(loan);
-              }
-            })
+            if (doc.data().date.toDate() >= new Date()) {
+              const docRef = ref.doc(doc.id)
+              docRef.collection('Applicants').doc(id).get().then(sdoc => {
+                if (sdoc.exists) {
+                  return;
+                }
+                else {
+                  let loan = new Job(doc.data().errandname, doc.data().category, doc.data().status, doc.data().client, doc.data().date.toDate(), doc.data().description, doc.data().time.toDate(), doc.data().endtime.toDate(), doc.id, doc.data().price);
+                  array.push(loan);
+                }
+              })
+            }
           } catch (error) { }
 
         });
@@ -197,7 +199,7 @@ export class JobERService {
       });
     });
   }
-  
+
   getAllErrandsCompleted(id: string): Observable<any> {
     return new Observable(observer => {
       // Read collection '/JobsCompleted'
@@ -208,22 +210,22 @@ export class JobERService {
         collection.forEach(doc => {
           // Add jobs into array if there's no error
           try {
-              const docRef = ref.doc(doc.id)
-              docRef.collection('Applicant').get().then(sdoc => {
-                sdoc.forEach(ssdoc => {
-                  if (ssdoc.id === id) {
-                    applied = true
-                  }
-                  else {
-                    applied = false
-                  }
+            const docRef = ref.doc(doc.id)
+            docRef.collection('Applicant').get().then(sdoc => {
+              sdoc.forEach(ssdoc => {
+                if (ssdoc.id === id) {
+                  applied = true
+                }
+                else {
+                  applied = false
+                }
 
-                  if (applied === true) {
-                    let loan = new Job(doc.data().errandname, doc.data().category, doc.data().status, doc.data().client, doc.data().date.toDate(), doc.data().description, doc.data().time.toDate(), doc.data().endtime.toDate(), doc.id, doc.data().price);
-                    array.push(loan);
-                  }
-                })
+                if (applied === true) {
+                  let loan = new Job(doc.data().errandname, doc.data().category, doc.data().status, doc.data().client, doc.data().date.toDate(), doc.data().description, doc.data().time.toDate(), doc.data().endtime.toDate(), doc.id, doc.data().price);
+                  array.push(loan);
+                }
               })
+            })
           } catch (error) { }
 
         });
@@ -242,22 +244,22 @@ export class JobERService {
         collection.forEach(doc => {
           // Add jobs into array if there's no error
           try {
-              const docRef = ref.doc(doc.id)
-              docRef.collection('Applicant').get().then(sdoc => {
-                sdoc.forEach(ssdoc => {
-                  if (ssdoc.id === id) {
-                    applied = true
-                  }
-                  else {
-                    applied = false
-                  }
+            const docRef = ref.doc(doc.id)
+            docRef.collection('Applicant').get().then(sdoc => {
+              sdoc.forEach(ssdoc => {
+                if (ssdoc.id === id) {
+                  applied = true
+                }
+                else {
+                  applied = false
+                }
 
-                  if (applied === true) {
-                    let loan = new Job(doc.data().errandname, doc.data().category, doc.data().status, doc.data().client, doc.data().date.toDate(), doc.data().description, doc.data().time.toDate(), doc.data().endtime.toDate(), doc.id, doc.data().price);
-                    array.push(loan);
-                  }
-                })
+                if (applied === true) {
+                  let loan = new Job(doc.data().errandname, doc.data().category, doc.data().status, doc.data().client, doc.data().date.toDate(), doc.data().description, doc.data().time.toDate(), doc.data().endtime.toDate(), doc.id, doc.data().price);
+                  array.push(loan);
+                }
               })
+            })
           } catch (error) { }
 
         });
