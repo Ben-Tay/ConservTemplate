415f3041661224a8972148bb01fe48a9f36e5797
astreagoh <76113005+astreagoh@users.noreply.github.com>
2021-02-08 08:50:01 +0800

    Merge branch 'master' of https://github.com/Ben-Tay/ConservTemplate into YQ

--- src/app/carddetails/carddetails.page.ts
+++ src/app/carddetails/carddetails.page.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ModalController, NavParams } from '@ionic/angular';
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
 import { PaymentcompletePage } from '../paymentcomplete/paymentcomplete.page';
 import { ErrandRunner } from '../shared/models/ErrandRunner';
 import { Job } from '../shared/models/Job';
 import { Payment } from '../shared/models/Payment';
 import { JobService } from '../shared/services/job.service';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-carddetails',
@@ -13,6 +15,7 @@ import { JobService } from '../shared/services/job.service';
   styleUrls: ['./carddetails.page.scss'],
 })
 export class CarddetailsPage implements OnInit {
+  CardNoForm: FormGroup;
   CardDetailsForm: FormGroup;
   cardNo: number;
   expiryDate: Date;
@@ -20,14 +23,31 @@ export class CarddetailsPage implements OnInit {
   bill: Payment;
   job: Job;
   applicant: ErrandRunner;
+  creditCardTypes = [
+    "Visa",
+    "AmericanExpress",
+    "MasterCard"
+  ]
 
-  constructor(private modalController: ModalController, public navParams: NavParams, public jobService: JobService) {
+
+  constructor(private modalController: ModalController, public navParams: NavParams, public jobService: JobService, private userService: UserService, private formBuilder: FormBuilder) {
     this.bill = navParams.get("fullamt")
     this.job = navParams.get('sJob')
     this.applicant = navParams.get('sApp')
+
+    this.
+
+    let securityCode = new FormControl('', Validators.compose([
+      Validators.minLength(3),
+      Validators.required,
+    ]))
   }
 
   ngOnInit() {
+    this.CardNoForm = this.formBuilder.group({
+      cardType:['Visa'],
+      creditCard:['',RxwebValidators.creditCard ({fieldName:'cardType'})]
+    })
   }
 
   async confirm() {
--- src/app/payment-errandrunner/payment-errandrunner.page.ts
+++ src/app/payment-errandrunner/payment-errandrunner.page.ts
@@ -42,7 +42,7 @@ export class PaymentErrandrunnerPage implements OnInit {
           })
         this.newJob = new Job(this.job.errandname, this.job.category, this.job.status, this.job.client, this.job.date, this.job.description, this.job.time, this.job.endtime, this.job.id, this.job.price, this.job.notification_time)
         this.applicant = new ErrandRunner(this.job.applicant[0].date, this.job.applicant[0].id, this.job.applicant[0].applicationstatus)
-        this.Bill = new Payment(this.errandId, this.job.price, parseFloat((this.job.price * 0.1).toFixed(2)), this.job.price - parseFloat((this.job.price * 0.1).toFixed(2)), '', 'Received')
+        this.Bill = new Payment(this.errandId, this.job.price, parseFloat((this.job.price * 0.07).toFixed(2)), this.job.price - parseFloat((this.job.price * 0.07).toFixed(2)), '', 'Received')
       })
 
     this.userService.observeAuthState(data => {
@@ -51,6 +51,7 @@ export class PaymentErrandrunnerPage implements OnInit {
   }
 
   ngOnInit() {
+    this.userService.showLoading();
   }
 
   async receive() {
@@ -65,5 +66,4 @@ export class PaymentErrandrunnerPage implements OnInit {
     });
     return await modal.present();
   }
-
 }
